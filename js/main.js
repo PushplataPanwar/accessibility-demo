6f3765f50f29b9a3f8d3681d9343b6a96b71e332
Sushil Kumar <sushil.kumar@infostride.com>
2024-04-17 15:32:15 +0530

    tets

--- js/main.js
+++ js/main.js
@@ -1,94 +1,150 @@
 var root = document.documentElement;
+var previousLinkHref = null;
+var nextLinkHref = null;
 // Function to update CSS variables
 function updateCSSVariables(primary, light, dark) {
-    root.style.setProperty('--primary', primary);
-    root.style.setProperty('--light', light);
-    root.style.setProperty('--dark', dark);
+  root.style.setProperty("--primary", primary);
+  root.style.setProperty("--light", light);
+  root.style.setProperty("--dark", dark);
 }
 
-
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 15); // 2000 milliseconds = 2 seconds
-    };
-    spinner();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 200); // 2000 milliseconds = 2 seconds
+  };
+  spinner();
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-// Example data for sidebar links
-const sidebarLinks = [
-    { href: "index.html", text: "Basic Accessibility", iconClass: "fa fa-tachometer-alt" },
-    { href: "#", text: "Indroduction", iconClass: "fa fa-laptop", dropdown: [
+  // Example data for sidebar links
+  const sidebarLinks = [
+    {
+      href: "index.html",
+      text: "Basic Accessibility",
+      iconClass: "fa fa-tachometer-alt",
+    },
+    {
+      href: "#",
+      text: "Indroduction",
+      iconClass: "fa fa-laptop",
+      dropdown: [
         { iconClass: "fa fa-laptop", href: "button.html", text: "Buttons" },
         { iconClass: "fa fa-laptop", href: "button.html", text: "Buttons" },
-    ]},
-    { href: "index.html", text: "1 Accessibility", iconClass: "fa fa-tachometer-alt" },
+      ],
+    },
+    {
+      href: "index.html",
+      text: "1 Accessibility",
+      iconClass: "fa fa-tachometer-alt",
+    },
+  ];
 
-];
-
-// Function to generate sidebar HTML
-function generateSidebarHTML(sidebarLinks) {
+  // Function to generate sidebar HTML
+  function generateSidebarHTML(sidebarLinks, currentPath) {
     return `
         <nav class="navbar bg-light navbar-light">
-            <a href="index.html" class="navbar-brand mx-4 mb-3">
+            <a href="index.html" class="navbar-brand mx-4 mb-3" tabindex="0">
                 <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>Accessibility </h3>
             </a>
-            <div class="d-flex align-items-center ms-4 mb-4">
+            <div class="d-flex align-items-center ms-4 mb-4" tabindex="0">
                 <div class="ms-3">
-                    <h6 class="mb-0">Accessibility Guidelines</h6>
+                    <h6 class="mb-0" tabindex="0">Accessibility Guidelines</h6>
                 </div>
             </div>
             <div class="navbar-nav w-100">
-                ${generateLinksHTML(sidebarLinks)}
+                ${generateLinksHTML(sidebarLinks, currentPath)}
             </div>
         </nav>
     `;
-}
+  }
 
-// Function to generate sidebar links HTML
-function generateLinksHTML(links) {
-    return links.map(link => {
+  function generateLinksHTML(links, currentPath) {
+    return links
+      .map((link, index, array) => {
         if (link.dropdown) {
-            return `
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="${link.iconClass} me-2"></i>${link.text}</a>
-                    <div class="dropdown-menu bg-transparent border-0">
-                        ${generateLinksHTML(link.dropdown)}
+          const isActiveDropdown = link.dropdown.some(
+            (subLink) => subLink.href === currentPath
+          );
+          const dropdownItemsHTML = link.dropdown
+            .map((subLink) => {
+              const isActive = subLink.href === currentPath;
+              if (isActive) {
+                previousLinkHref = index > 0 ? array[index - 1].href : null;
+                nextLinkHref =
+                  index < array.length - 1 ? array[index + 1].href : null;
+              }
+              return `<a href="${subLink.href}" class="dropdown-item ${
+                isActive ? "active" : ""
+              }" tabindex="0">${subLink.text}</a>`;
+            })
+            .join("");
+
+          return `
+                <div class="nav-item dropdown ${
+                  isActiveDropdown ? "active" : ""
+                }" tabindex="0">
+                    <a href="#" class="nav-link dropdown-toggle ${
+                      isActiveDropdown ? "active" : ""
+                    }" data-bs-toggle="dropdown" tabindex="0"><i class="${
+            link.iconClass
+          } me-2"></i>${link.text}</a>
+                    <div class="dropdown-menu bg-transparent border-0 ${
+                      isActiveDropdown ? "show" : ""
+                    }">
+                        ${dropdownItemsHTML}
                     </div>
                 </div>
             `;
         } else {
-            return `<a href="${link.href}" class="nav-item nav-link"><i class="${link.iconClass} me-2"></i>${link.text}</a>`;
+          const isActive = link.href === currentPath;
+          if (isActive && index > 0) {
+            previousLinkHref = array[index - 1].href;
+          }
+          if (isActive && index < array.length - 1) {
+            nextLinkHref = array[index + 1].href;
+          }
+          return `<a href="${
+            link.href
+          }" tabindex="0" class="nav-item nav-link ${
+            isActive ? "active" : ""
+          }"><i class="${link.iconClass} me-2"></i>${link.text}</a>`;
         }
-    }).join('');
-}
+      })
+      .join("");
+  }
+  $(document).ready(function () {
+    let currentPath = window.location.pathname;
+    currentPath = currentPath.replace(/^\/|\/index.html$/g, "");
+    $(".sidebar_container").html(
+      generateSidebarHTML(sidebarLinks, currentPath)
+    );
+    $(".header_container").html(header);
+    console.log(nextLinkHref, previousLinkHref);
+  });
 
-// Example usage:
-// const sidebarContainer = document.getElementById('sidebar-container');
-// sidebarContainer.innerHTML = generateSidebarHTML(sidebarLinks);
+  // Example usage:
+  // const sidebarContainer = document.getElementById('sidebar-container');
+  // sidebarContainer.innerHTML = generateSidebarHTML(sidebarLinks);
 
-
-    const header = `<nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
+  const header = `<nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
 <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
     <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
 </a>
@@ -171,198 +227,186 @@ function generateLinksHTML(links) {
         </div>
     </div>
 </div>
-</nav>`
+</nav>`;
 
-
-
-
-    $(document).ready(function () {
-        $('.sidebar_container').html(generateSidebarHTML(sidebarLinks));
-        $('.header_container').html(header);
-        header
+  // Sidebar Toggler
+  $(document).ready(function () {
+    $(".sidebar-toggler").click(function () {
+      $(".sidebar, .content").toggleClass("open");
+      return false;
     });
+  });
 
+  // Progress Bar
+  $(".pg-bar").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
 
-    // Sidebar Toggler
-    $(document).ready(function () {
-        $('.sidebar-toggler').click(function () {
-            $('.sidebar, .content').toggleClass("open");
-            return false;
-        });
-    });
+  // Calender
+  $("#calender").datetimepicker({
+    inline: true,
+    format: "L",
+  });
 
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav: false,
+  });
 
-    // Progress Bar
-    $('.pg-bar').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, { offset: '80%' });
-
-
-    // Calender
-    $('#calender').datetimepicker({
-        inline: true,
-        format: 'L'
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav: false
-    });
-
-
-    // Worldwide Sales Chart
-    var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
-    var myChart1 = new Chart(ctx1, {
-        type: "bar",
-        data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
-                label: "USA",
-                data: [15, 30, 55, 65, 60, 80, 95],
-                backgroundColor: "rgba(0, 156, 255, .7)"
-            },
-            {
-                label: "UK",
-                data: [8, 35, 40, 60, 70, 55, 75],
-                backgroundColor: "rgba(0, 156, 255, .5)"
-            },
-            {
-                label: "AU",
-                data: [12, 25, 45, 55, 65, 70, 60],
-                backgroundColor: "rgba(0, 156, 255, .3)"
-            }
-            ]
+  // Worldwide Sales Chart
+  var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
+  var myChart1 = new Chart(ctx1, {
+    type: "bar",
+    data: {
+      labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+      datasets: [
+        {
+          label: "USA",
+          data: [15, 30, 55, 65, 60, 80, 95],
+          backgroundColor: "rgba(0, 156, 255, .7)",
         },
-        options: {
-            responsive: true
-        }
-    });
-
-
-    // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
-    var myChart2 = new Chart(ctx2, {
-        type: "line",
-        data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
-                label: "Salse",
-                data: [15, 30, 55, 45, 70, 65, 85],
-                backgroundColor: "rgba(0, 156, 255, .5)",
-                fill: true
-            },
-            {
-                label: "Revenue",
-                data: [99, 135, 170, 130, 190, 180, 270],
-                backgroundColor: "rgba(0, 156, 255, .3)",
-                fill: true
-            }
-            ]
+        {
+          label: "UK",
+          data: [8, 35, 40, 60, 70, 55, 75],
+          backgroundColor: "rgba(0, 156, 255, .5)",
         },
-        options: {
-            responsive: true
-        }
-    });
-
-
-
-    // Single Line Chart
-    var ctx3 = $("#line-chart").get(0).getContext("2d");
-    var myChart3 = new Chart(ctx3, {
-        type: "line",
-        data: {
-            labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
-            datasets: [{
-                label: "Salse",
-                fill: false,
-                backgroundColor: "rgba(0, 156, 255, .3)",
-                data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
-            }]
+        {
+          label: "AU",
+          data: [12, 25, 45, 55, 65, 70, 60],
+          backgroundColor: "rgba(0, 156, 255, .3)",
         },
-        options: {
-            responsive: true
-        }
-    });
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 
-
-    // Single Bar Chart
-    var ctx4 = $("#bar-chart").get(0).getContext("2d");
-    var myChart4 = new Chart(ctx4, {
-        type: "bar",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(0, 156, 255, .7)",
-                    "rgba(0, 156, 255, .6)",
-                    "rgba(0, 156, 255, .5)",
-                    "rgba(0, 156, 255, .4)",
-                    "rgba(0, 156, 255, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
+  // Salse & Revenue Chart
+  var ctx2 = $("#salse-revenue").get(0).getContext("2d");
+  var myChart2 = new Chart(ctx2, {
+    type: "line",
+    data: {
+      labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+      datasets: [
+        {
+          label: "Salse",
+          data: [15, 30, 55, 45, 70, 65, 85],
+          backgroundColor: "rgba(0, 156, 255, .5)",
+          fill: true,
         },
-        options: {
-            responsive: true
-        }
-    });
-
-
-    // Pie Chart
-    var ctx5 = $("#pie-chart").get(0).getContext("2d");
-    var myChart5 = new Chart(ctx5, {
-        type: "pie",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(0, 156, 255, .7)",
-                    "rgba(0, 156, 255, .6)",
-                    "rgba(0, 156, 255, .5)",
-                    "rgba(0, 156, 255, .4)",
-                    "rgba(0, 156, 255, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
+        {
+          label: "Revenue",
+          data: [99, 135, 170, 130, 190, 180, 270],
+          backgroundColor: "rgba(0, 156, 255, .3)",
+          fill: true,
         },
-        options: {
-            responsive: true
-        }
-    });
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 
-
-    // Doughnut Chart
-    var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
-    var myChart6 = new Chart(ctx6, {
-        type: "doughnut",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(0, 156, 255, .7)",
-                    "rgba(0, 156, 255, .6)",
-                    "rgba(0, 156, 255, .5)",
-                    "rgba(0, 156, 255, .4)",
-                    "rgba(0, 156, 255, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
+  // Single Line Chart
+  var ctx3 = $("#line-chart").get(0).getContext("2d");
+  var myChart3 = new Chart(ctx3, {
+    type: "line",
+    data: {
+      labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+      datasets: [
+        {
+          label: "Salse",
+          fill: false,
+          backgroundColor: "rgba(0, 156, 255, .3)",
+          data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15],
         },
-        options: {
-            responsive: true
-        }
-    });
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 
+  // Single Bar Chart
+  var ctx4 = $("#bar-chart").get(0).getContext("2d");
+  var myChart4 = new Chart(ctx4, {
+    type: "bar",
+    data: {
+      labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+      datasets: [
+        {
+          backgroundColor: [
+            "rgba(0, 156, 255, .7)",
+            "rgba(0, 156, 255, .6)",
+            "rgba(0, 156, 255, .5)",
+            "rgba(0, 156, 255, .4)",
+            "rgba(0, 156, 255, .3)",
+          ],
+          data: [55, 49, 44, 24, 15],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 
+  // Pie Chart
+  var ctx5 = $("#pie-chart").get(0).getContext("2d");
+  var myChart5 = new Chart(ctx5, {
+    type: "pie",
+    data: {
+      labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+      datasets: [
+        {
+          backgroundColor: [
+            "rgba(0, 156, 255, .7)",
+            "rgba(0, 156, 255, .6)",
+            "rgba(0, 156, 255, .5)",
+            "rgba(0, 156, 255, .4)",
+            "rgba(0, 156, 255, .3)",
+          ],
+          data: [55, 49, 44, 24, 15],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 
-
+  // Doughnut Chart
+  var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
+  var myChart6 = new Chart(ctx6, {
+    type: "doughnut",
+    data: {
+      labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+      datasets: [
+        {
+          backgroundColor: [
+            "rgba(0, 156, 255, .7)",
+            "rgba(0, 156, 255, .6)",
+            "rgba(0, 156, 255, .5)",
+            "rgba(0, 156, 255, .4)",
+            "rgba(0, 156, 255, .3)",
+          ],
+          data: [55, 49, 44, 24, 15],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 })(jQuery);
-
